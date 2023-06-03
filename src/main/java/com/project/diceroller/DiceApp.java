06267afc35753b1a872832819afbb689b7eb05db
Anshul0707 <anshul.markwade786@gmail.com>
2023-06-03 12:11:29 +0530

    DiceApp

--- src/main/java/com/project/diceroller/DiceApp.java
+++ src/main/java/com/project/diceroller/DiceApp.java
@@ -1,22 +1,26 @@
-package com.project;
+package com.project.diceroller;
 
+import com.project.diceroller.controller.DiceRollController;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan
 @Slf4j
 public class DiceApp implements CommandLineRunner {
     @Autowired
-    Controller controller;
+    DiceRollController diceRollController;
 
     public static void main(String[] args) {
         SpringApplication.run(DiceApp.class, args);
     }
     @Override
     public void run(String... args) throws Exception {
-        controller.hello();
+        diceRollController.hello();
+        diceRollController.processRollForm("4D6 + 3D4");
     }
 }
